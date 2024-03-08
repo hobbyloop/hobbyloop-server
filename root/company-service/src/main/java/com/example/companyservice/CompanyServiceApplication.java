67ee5e2bc1e73bee9210cf61330859d559e5305f
keeeeeey <sseioul@naver.com>
2024-03-08 12:23:17 +0900

    feat: 업체 정보 등록 신청 기능 구현

--- root/company-service/src/main/java/com/example/companyservice/CompanyServiceApplication.java
+++ root/company-service/src/main/java/com/example/companyservice/CompanyServiceApplication.java
@@ -3,10 +3,12 @@ package com.example.companyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableDiscoveryClient
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"com.example.companyservice"})
 public class CompanyServiceApplication {
 
--- root/pay-service/src/main/java/com/example/payservice/entity/CompanyPaymentType.java
+++ root/pay-service/src/main/java/com/example/payservice/entity/CompanyPaymentType.java
@@ -9,14 +9,23 @@ import java.util.Arrays;
 @AllArgsConstructor
 public enum CompanyPaymentType {
 
-    FIX(1), // 정률제
-    FLAT(2); // 정액제
+    FIX(1, "정률제"), // 정률제
+    FLAT(2, "정액제"); // 정액제
 
     private final int paymentType;
+
+    private final String name;
 
     public static CompanyPaymentType findByValue(final int paymentType) {
         return Arrays.stream(CompanyPaymentType.values())
                 .filter(e -> e.paymentType == paymentType)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("유효하지 않은 결제 방식 입니다."));
+    }
+
+    public static CompanyPaymentType findByName(final String name) {
+        return Arrays.stream(CompanyPaymentType.values())
+                .filter(e -> e.name.equals(name))
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("유효하지 않은 결제 방식 입니다."));
     }
