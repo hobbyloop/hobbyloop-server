9b5167a70d0fd66cc18ab339ee1aa95036fde282
keeeeeey <sseioul@naver.com>
2024-03-20 13:14:57 +0900

    fix: 시설 정보 조회시 이미지 링크 추가

--- root/company-service/src/main/java/com/example/companyservice/dto/response/CenterCreateResponseDto.java
+++ root/company-service/src/main/java/com/example/companyservice/dto/response/CenterCreateResponseDto.java
@@ -24,17 +24,23 @@ public class CenterCreateResponseDto {
 
     private String kakaoLink;
 
+    private String logoImageUrl;
+
     private List<HourResponseDto> operatingHourList;
 
     private List<HourResponseDto> breakHourList;
 
-    public static CenterCreateResponseDto of(Center center, List<HourResponseDto> operatingHourList, List<HourResponseDto> breakHourList) {
+    public static CenterCreateResponseDto of(Center center,
+                                             String logoImageUrl,
+                                             List<HourResponseDto> operatingHourList,
+                                             List<HourResponseDto> breakHourList) {
         return CenterCreateResponseDto.builder()
                 .centerId(center.getId())
                 .centerName(center.getCenterName())
                 .address(center.getAddress())
                 .contact(center.getContact())
                 .kakaoLink(center.getKakaoLink())
+                .logoImageUrl(logoImageUrl)
                 .operatingHourList(operatingHourList)
                 .breakHourList(breakHourList)
                 .build();
