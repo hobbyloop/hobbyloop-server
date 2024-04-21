9bf416e0c895d919678dbda315a9f3b43d6df2ab
toneofrain <toneofrain@gmail.com>
2024-04-22 04:35:17 +0900

    refactor: optimize import (#71)

--- root/company-service/src/main/java/com/example/companyservice/CompanyServiceApplication.java
+++ root/company-service/src/main/java/com/example/companyservice/CompanyServiceApplication.java
@@ -1,13 +1,9 @@
 package com.example.companyservice;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableDiscoveryClient
 @EnableFeignClients
--- root/company-service/src/main/java/com/example/companyservice/config/JpaConfig.java
+++ root/company-service/src/main/java/com/example/companyservice/config/JpaConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import jakarta.persistence.Id;
-
 @Configuration
 @EnableJpaRepositories(basePackages = "com.example.companyservice")
 @EnableJpaAuditing
