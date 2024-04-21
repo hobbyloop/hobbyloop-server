40a32afd8122dd5b28a115f0ee64b4470b297917
samkimpepper <62634206+samkimpepper@users.noreply.github.com>
2024-04-21 15:02:56 +0900

    deploy: 각 서버 start.sh 수정

--- root/api-gateway-server/scripts/start.sh
+++ root/api-gateway-server/scripts/start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Api Gateway Server 설정
-API_GATEWAY_SERVER_ROOT="/home/ubuntu/app/api-gateway"
+API_GATEWAY_SERVER_ROOT="/home/ubuntu/app/api-gateway-server"
+API_GATEWAY_LATEST_JAR=$(ls -t /home/ubuntu/app/api-gateway-server/build/libs/*.jar | grep -v "plain" | head -1)
 API_GATEWAY_SERVER_JAR="$API_GATEWAY_SERVER_ROOT/api-gateway-server.jar"
 API_GATEWAY_SERVER_APP_LOG="$API_GATEWAY_SERVER_ROOT/api-gateway-server.log"
 API_GATEWAY_SERVER_ERROR_LOG="$API_GATEWAY_SERVER_ROOT/api-gateway-server-error.log"
@@ -11,7 +12,7 @@ TIME_NOW=$(date +%c)
 
 # API Gateway Server 배포 및 실행
 echo "$TIME_NOW > $API_GATEWAY_SERVER_JAR 파일 복사" >> $DEPLOY_LOG
-cp $API_GATEWAY_SERVER_ROOT/build/libs/*.jar $API_GATEWAY_SERVER_JAR
+cp "$API_GATEWAY_LATEST_JAR" "$API_GATEWAY_SERVER_JAR"
 
 echo "$TIME_NOW > $API_GATEWAY_SERVER_JAR 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $API_GATEWAY_SERVER_JAR > $API_GATEWAY_SERVER_APP_LOG 2> $API_GATEWAY_SERVER_ERROR_LOG &
--- root/discovery-server/scripts/start.sh
+++ root/discovery-server/scripts/start.sh
@@ -2,6 +2,7 @@
 
 # Discovery Server 설정
 DISCOVERY_SERVER_ROOT="/home/ubuntu/app/discovery"
+DISCOVERY_LATEST_JAR=$(ls -t /home/ubuntu/app/discovery-server/build/libs/*.jar | grep -v "plain" | head -1)
 DISCOVERY_SERVER_JAR="$DISCOVERY_SERVER_ROOT/discovery-server.jar"
 DISCOVERY_SERVER_APP_LOG="$DISCOVERY_SERVER_ROOT/discovery-server.log"
 DISCOVERY_SERVER_ERROR_LOG="$DISCOVERY_SERVER_ROOT/discovery-server-error.log"
@@ -11,7 +12,7 @@ TIME_NOW=$(date +%c)
 
 # Discovery Server 배포 및 실행
 echo "$TIME_NOW > $DISCOVERY_SERVER_JAR 파일 복사" >> $DEPLOY_LOG
-cp $DISCOVERY_SERVER_ROOT/build/libs/*.jar $DISCOVERY_SERVER_JAR
+cp "$DISCOVERY_LATEST_JAR" "$DISCOVERY_SERVER_JAR"
 
 echo "$TIME_NOW > $DISCOVERY_SERVER_JAR 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $DISCOVERY_SERVER_JAR > $DISCOVERY_SERVER_APP_LOG 2> $DISCOVERY_SERVER_ERROR_LOG &
