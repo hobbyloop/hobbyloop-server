68aaa9fc9f1c35102429f0b8afc28fb05af64b64
toneofrain <toneofrain@gmail.com>
2024-04-23 08:03:34 +0900

    refactor: refactor instructor repository to custom interface (#71)
    remove unnecessary methods in crudrepository

--- root/company-service/src/main/java/com/example/companyservice/instructor/infrastructure/persistence/InstructorOauthRepository.java
+++ root/company-service/src/main/java/com/example/companyservice/instructor/infrastructure/persistence/InstructorOauthRepository.java
@@ -1,8 +1,9 @@
 package com.example.companyservice.instructor.infrastructure.persistence;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import com.example.companyservice.instructor.domain.InstructorOauth;
 
-public interface InstructorOauthRepository extends CrudRepository<InstructorOauth, Long> {
+public interface InstructorOauthRepository extends Repository<InstructorOauth, Long> {
+	<T extends InstructorOauth> T save(T entity);
 }
--- root/company-service/src/main/java/com/example/companyservice/instructor/infrastructure/persistence/InstructorRepository.java
+++ root/company-service/src/main/java/com/example/companyservice/instructor/infrastructure/persistence/InstructorRepository.java
@@ -1,8 +1,9 @@
 package com.example.companyservice.instructor.infrastructure.persistence;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import com.example.companyservice.instructor.domain.Instructor;
 
-public interface InstructorRepository extends CrudRepository<Instructor, Long> {
+public interface InstructorRepository extends Repository<Instructor, Long> {
+	<T extends Instructor> T save(T entity);
 }
