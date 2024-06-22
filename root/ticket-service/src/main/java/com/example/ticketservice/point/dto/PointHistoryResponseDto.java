f05b5a25aec49443fabdd041981eb7562693c284
samkimpepper <62634206+samkimpepper@users.noreply.github.com>
2024-06-23 08:35:10 +0900

    add: 포인트 컨트롤러 스웨거 적용

--- root/ticket-service/src/main/java/com/example/ticketservice/point/dto/PointHistoryResponseDto.java
+++ root/ticket-service/src/main/java/com/example/ticketservice/point/dto/PointHistoryResponseDto.java
@@ -2,6 +2,7 @@ package com.example.ticketservice.point.dto;
 
 import com.example.ticketservice.point.entity.PointHistory;
 import com.example.ticketservice.point.entity.enums.PointTypeEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,10 +15,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PointHistoryResponseDto {
+    @Schema(description = "포인트 유형(EARN, USE)", example = "EARN")
     private String type;
+
+    @Schema(description = "적립/사용 금액", example = "2500")
     private Long amount;
+
+    @Schema(description = "적립/사용 시의 잔액", example = "4200")
     private Long balance;
+
+    @Schema(description = "설명", example = "하비루프 스튜디오 이용권 사용")
     private String description;
+
+    @Schema(description = "적립/사용 시점")
     private LocalDateTime createdAt;
 
     public static PointHistoryResponseDto from(PointHistory pointHistory) {
